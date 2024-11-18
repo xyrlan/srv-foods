e1783ca45a49c8cbdbeb3914190fbe19b5dd8393
xyrlan <lobatopedro68@gmail.com>
2024-11-18 12:28:14 -0400

    fix import urlFor customersection

--- components/sections/customersSection.jsx
+++ components/sections/customersSection.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { motion } from "framer-motion";
 import Link from 'next/link';
 import Image from 'next/image';
+import { urlFor } from '@/sanity/lib/image';
 
 const CustomersSection = ({ clients }) => {
   return (
