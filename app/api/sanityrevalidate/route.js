2445df4235d1efe4188165d266dfda935650e0f2
xyrlan <lobatopedro68@gmail.com>
2024-11-18 15:06:50 -0400

    udpate

--- app/api/sanityrevalidate/route.js
+++ app/api/sanityrevalidate/route.js
@@ -3,7 +3,11 @@ import { NextResponse } from "next/server";
 import { parseBody } from "next-sanity/webhook";
 import { webhookSecret } from "@/sanity/env";
 
-export async function POST(req) {
+export async function handler(req) {
+  // Verifica se o método da requisição é POST
+  if (req.method !== "POST") {
+    return NextResponse.json({ error: "Method Not Allowed" }, { status: 405 });
+  }
   try {
     // Parseia o corpo da requisição e valida a assinatura
     const { body, isValidSignature } = await parseBody(
