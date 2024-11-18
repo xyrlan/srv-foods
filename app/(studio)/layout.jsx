c6f1c0169f9f55dcf03a1ed327bd1e0fb0b47b57
xyrlan <lobatopedro68@gmail.com>
2024-11-18 13:50:36 -0400

    new structure layout

--- app/(studio)/layout.jsx
+++ app/(studio)/layout.jsx
@@ -1,13 +1,13 @@
 import localFont from "next/font/local";
-import "./globals.css";
+import "../globals.css";
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
+  src: "../fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
+  src: "../fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
 });
@@ -43,7 +43,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased relative dark:bg-stone-900 bg-white`}
       >
